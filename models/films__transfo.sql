71114abba5ad3cf94a94a1beab373de4b6188945
Omar <sk8pro@hotmail.fr>
2023-05-10 21:53:42 +0200

    change sql and config update

--- models/films__transfo.sql
+++ models/films__transfo.sql
@@ -1,41 +1,54 @@
--- Créer une vue temporaire pour la table title_basics_truncated de MySQL
-WITH mysql_title_basics AS (
-  SELECT *
-  FROM {{ ref('films.title_basics_truncated') }}
-),
+-- -- Créer une vue temporaire pour la table title_basics_truncated de MySQL
+-- WITH mysql_title_basics AS (
+--   SELECT *
+--   FROM {{ ref('films.title_basics_truncated') }}
+-- ),
 
--- Mettre la colonne primaryTitle en majuscule
-uppercase_primarytitle AS (
-  SELECT 
-    tconst, 
-    UPPER(primaryTitle) AS primaryTitle,
-    originalTitle, 
-    isAdult, 
-    startYear, 
-    endYear, 
-    runtimeMinutes,
-    genres
-  FROM mysql_title_basics
-)
+-- -- Mettre la colonne primaryTitle en majuscule
+-- uppercase_primarytitle AS (
+--   SELECT 
+--     tconst, 
+--     UPPER(primaryTitle) AS primaryTitle,
+--     originalTitle, 
+--     isAdult, 
+--     startYear, 
+--     endYear, 
+--     runtimeMinutes,
+--     genres
+--   FROM mysql_title_basics
+-- )
 
--- Insérer les données transformées dans la table title_basics de PostgreSQL
-INSERT INTO {{ ref('postgres.title_basics') }} (
+-- -- Insérer les données transformées dans la table title_basics de PostgreSQL
+-- INSERT INTO {{ ref('postgres.title_basics') }} (
+--   tconst,
+--   primaryTitle,
+--   originalTitle,
+--   isAdult,
+--   startYear,
+--   endYear,
+--   runtimeMinutes,
+--   genres
+-- )
+-- SELECT 
+--   tconst,
+--   primaryTitle,
+--   originalTitle,
+--   isAdult,
+--   startYear,
+--   endYear,
+--   runtimeMinutes,
+--   genres
+-- FROM uppercase_primarytitle;
+
+
+SELECT
   tconst,
-  primaryTitle,
+  titleType,
+  UPPER(primaryTitle) AS primaryTitle,
   originalTitle,
   isAdult,
   startYear,
   endYear,
   runtimeMinutes,
   genres
-)
-SELECT 
-  tconst,
-  primaryTitle,
-  originalTitle,
-  isAdult,
-  startYear,
-  endYear,
-  runtimeMinutes,
-  genres
-FROM uppercase_primarytitle;
+FROM {{ source('title_basics_truncated') }}
